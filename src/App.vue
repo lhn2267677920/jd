4d4d09a3f1aa6a70f048ee18312b05377b2969a2
lhn2267677920 <2267677920@qq.com>
2022-09-09 13:56:01 +0800

    页面布局

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <views-home/>
+  </div>
+</template>
+
+<script>
+import ViewsHome from "@/views/home/ViewsHome";
+
+export default {
+  name: "App",
+  components: {ViewsHome}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/ViewsHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <home-header></home-header>
+    <home-content></home-content>
+  </div>
+  <home-footer></home-footer>
+</template>
+
+<script>
+
+import HomeHeader from "@/views/home/HomeHeader";
+import HomeContent from "@/views/home/HomeContent";
+import HomeFooter from "@/views/home/HomeFooter";
+
+export default {
+  name: "ViewsHome",
+  components: {HomeFooter, HomeContent, HomeHeader}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/viriables.scss";
+@import "@/style/mixins.scss";
+
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem .2rem .18rem;
+}
+
+
+</style>
